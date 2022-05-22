51a09cbac665a05d80086aae8694e548980c2a2b
codewithshamim <shamimislamonline@gmail.com>
2022-05-23 00:38:48 +0600

    find specific tool item by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 var jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,14 @@ async function run() {
       const query = {};
       const tools = await toolCollection.find(query).toArray();
       res.send(tools);
+    });
+
+    // find specific data
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
     });
   } finally {
     // await client.close();
