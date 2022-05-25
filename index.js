5d2d0fef578b6a280068f4ee86437aea2ad8eeb0
codewithshamim <shamimislamonline@gmail.com>
2022-05-25 16:08:42 +0600

    create payment intent

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const cors = require("cors");
 var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
+
+console.log(stripe);
 
 // use middleware
 app.use(cors());
@@ -37,7 +40,6 @@ async function run() {
     //generate token
     app.get("/token/:email", (req, res) => {
       const email = req.params.email;
-      console.log(email);
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_PRIVATE,
@@ -69,12 +71,20 @@ async function run() {
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
-    // get all order
+    // get specific user order
     app.get("/order/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const orders = await orderCollection.find(query).toArray();
       res.send(orders);
+    });
+
+    // get specific order by id
+    app.get("/orderItem/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollection.findOne(query);
+      res.send(order);
     });
 
     // delete specific order
@@ -120,6 +130,18 @@ async function run() {
       const query = { email: email };
       const result = await profileCollection.findOne(query);
       res.send(result);
+    });
+
+    // create payment intent for stripe
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
     });
   } finally {
     // await client.close();
