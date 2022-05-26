5ce2feeed4b5f811d93f77e8d01feeb18fab9718
codewithshamim <shamimislamonline@gmail.com>
2022-05-26 10:29:46 +0600

    veify jwt

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(stripe);
-
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// _____create middleware for verify jwt token_________
+const verifyJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c4pos.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -36,18 +51,30 @@ async function run() {
     const profileCollection = client
       .db("refrigerator_instruments")
       .collection("profiles");
+    const userCollection = client
+      .db("refrigerator_instruments")
+      .collection("users");
 
-    //generate token
-    app.get("/token/:email", (req, res) => {
+    // create user & generate token
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
+      const user = req.body;
+      console.log(user);
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          email: user.newUser,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      // generate jwt token
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_PRIVATE,
-        {
-          expiresIn: "2h",
-        }
+        { expiresIn: "3h" }
       );
-      res.send({ token: token });
+      res.send({ result, token });
     });
 
     // find all tools
